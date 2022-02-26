bcfd118f16f99e736a4bb04cdc62a39e17ea2fd4
ttaerrim <ltr0121@naver.com>
2022-02-27 02:08:27 +0900

    [Modify] 캐러셀 정보 표시 & 자동 캐러셀

--- src/component/Carousel/Carousel.js
+++ src/component/Carousel/Carousel.js
@@ -22,6 +22,8 @@ const Carousel = () => {
 
   const SLIDE_MARGIN = 24;
 
+  // currentslide = (slideState.number % carouselData.length) + 1
+
   const setInitialPosition = () => {
     imgWidth === undefined
       ? (slideRef.current.style.transform = `translateX(-${
@@ -100,6 +102,7 @@ const Carousel = () => {
     slideRef.current.style.transition = slideState.hasMotion
       ? "all 500ms ease 0s"
       : "";
+    setTimeout(handleBannerRight, 4000);
   }, [slideState, imgWidth]);
 
   return (
@@ -117,6 +120,36 @@ const Carousel = () => {
                       alt="slide"
                       ref={imgRef}
                     />
+                    <div
+                      className={
+                        (slideState.number % carouselData.length) + 1 !==
+                          data.id && "unactive-slideImage"
+                      }
+                    />
+
+                    <div
+                      className={
+                        (slideState.number % carouselData.length) + 1 ===
+                        data.id
+                          ? "active-infomation"
+                          : "infomation"
+                      }
+                    >
+                      <h2>{data.title}</h2>
+                      <h3>{data.content}</h3>
+                      <hr />
+                      <a>
+                        <span>
+                          바로가기
+                          <svg
+                            class="SvgIcon_SvgIcon__root__svg__DKYBi"
+                            viewBox="0 0 18 18"
+                          >
+                            <path d="m11.955 9-5.978 5.977a.563.563 0 0 0 .796.796l6.375-6.375a.563.563 0 0 0 0-.796L6.773 2.227a.562.562 0 1 0-.796.796L11.955 9z"></path>
+                          </svg>
+                        </span>
+                      </a>
+                    </div>
                   </div>
                 );
               })}
